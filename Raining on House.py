228fefe18569b7aaffa58e80f22b9c27c10e7629
Nafis Rayan <90589446+NafisRayan@users.noreply.github.com>
2024-02-14 19:55:59 +0600

    Update Raining on House.py

--- Raining on House.py
+++ Raining on House.py
@@ -6,6 +6,10 @@ import numpy as np
 
 raindrop_count = 1000
 rain_speed = 0.01
+rain_angle = 0.0
+house_color = [1.0, 0.5, 0.31]
+day_night_cycle = 0.0
+color_step = 0.01  # initialize color_step variable
 
 def draw_raindrops():
     glColor3f(0.0, 0.0, 1.0)
@@ -13,6 +17,7 @@ def draw_raindrops():
     for i in range(raindrop_count):
         glPushMatrix()
         glTranslatef(raindrops[i][0], raindrops[i][1], 0)
+        glRotatef(rain_angle, 0.0, 0.0, 1.0)
 
         glBegin(GL_LINES)
         glVertex2f(0, 0)
@@ -25,11 +30,8 @@ def draw_raindrops():
 
         glPopMatrix()
 
-
-
 def draw_house():
-    # Draw the walls
-    glColor3f(1.0, 0.5, 0.31)
+    glColor3fv(house_color)
     glBegin(GL_LINE_LOOP)
     glVertex2f(-0.5, -0.5)
     glVertex2f(-0.5, 0.5)
@@ -37,7 +39,6 @@ def draw_house():
     glVertex2f(0.5, -0.5)
     glEnd()
 
-    # Draw the roof
     glColor3f(0.7, 0.2, 0.0)
     glBegin(GL_TRIANGLES)
     glVertex2f(-0.5, 0.5)
@@ -45,7 +46,6 @@ def draw_house():
     glVertex2f(0.0, 1.0)
     glEnd()
 
-    # Draw the door
     glColor3f(0.0, 0.0, 1.0)
     glBegin(GL_QUADS)
     glVertex2f(-0.2, -0.5)
@@ -54,7 +54,6 @@ def draw_house():
     glVertex2f(0.2, -0.5)
     glEnd()
 
-    # Draw the window
     glColor3f(0.0, 0.0, 1.0)
     glBegin(GL_QUADS)
     glVertex2f(-0.3, 0.2)
@@ -64,7 +63,7 @@ def draw_house():
     glEnd()
 
 def update():
-    global raindrop_count
+    global raindrop_count, rain_angle, house_color, day_night_cycle, color_step
 
     for i in range(raindrop_count):
         raindrops[i][1] -= rain_speed
@@ -72,6 +71,19 @@ def update():
         if raindrops[i][1] < -1.0:
             raindrops[i][1] = 1.0 + random.uniform(0.0, 0.1)
             raindrops[i][0] = random.uniform(-0.8, 0.8)
+
+    rain_angle += 0.01
+    if rain_angle > 10.0 or rain_angle < -10.0:
+        rain_angle = 0.0
+
+    day_night_cycle += color_step
+    if day_night_cycle > 1.0 or day_night_cycle < 0.0:
+        color_step = -color_step
+
+    r = 1.0 - day_night_cycle
+    g = day_night_cycle * 0.5
+    b = day_night_cycle * 0.31
+    house_color = [r, g, b]
 
 def display():
     glClear(GL_COLOR_BUFFER_BIT)
@@ -87,6 +99,18 @@ def idle():
     update()
     glutPostRedisplay()
 
+def key_press(key, x, y):
+    global rain_angle, color_step
+
+    if key == b'Left':
+        rain_angle -= 0.1
+    elif key == b'Right':
+        rain_angle += 0.1
+    elif key == b'd':  # Change house color to dark
+        color_step = 0.01
+    elif key == b'l':  # Change house color to light
+        color_step = -0.01
+
 def main():
     global raindrops
     raindrops = [[random.uniform(-1.0, 1.0), random.uniform(-1.0, 1.0)] for _ in range(raindrop_count)]
@@ -97,6 +121,7 @@ def main():
     glutCreateWindow(b"House Drawing")
     glutDisplayFunc(display)
     glutIdleFunc(idle)
+    glutKeyboardFunc(key_press)
     glutMainLoop()
 
 main()
